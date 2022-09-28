e96003c512ddb6e5be1ca80942acc1b923118696
Pekeniojimi <mato45495@gmail.com>
2022-09-28 09:38:04 +0200

    Commit 1.1

---
+++ src/main.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int menu_int;
+    char response;
+
+
+    printf("[2022] SUCEM S.L Wifi Collector\n\n");
+    printf("[ 1] wificollector_quit\n");
+    printf("[ 2] wificollector_collect\n");
+    printf("[ 3] wificollector_show_data_one_network\n");
+    printf("[ 4] wificollector_select_best\n");
+    printf("[ 5] wificollector_select_worst\n");
+    printf("[ 6] wificollector_delete_net\n");
+    printf("[ 7] wificollector_sort\n");
+    printf("[ 8] wificollector_export\n");
+    printf("[ 9] wificollector_import\n");
+    printf("[10] wificollector_display\n");
+    printf("[11] wificollector_display_all\n\n");
+    printf("Option: ");
+    
+    scanf("%i", &menu_int);
+   // printf("%i",menu_int);
+
+  /* switch (menu_int)
+   {
+   case 1:
+    printf("Are you sure you want to exit? [y/N]:\n");
+    scanf("%c",&response)
+    if(response == ('y' || 'n'))
+    {
+
+    }
+    break;
+   
+   default:
+    break;
+   }*/
+
+    return 0;
+
+}
